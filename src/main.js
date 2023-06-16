989450299c505d38e12be320ca79ec7fc2b93154
DDundara <116847625+DDundara@users.noreply.github.com>
2023-06-16 22:28:49 +0200

    inicijalizacija css datoteke

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import App from './App.vue'
 // createApp(App).use(VueSession);
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/css/main.css';
 import router from './router'
+
 
 
 // register jw pagination component globally
